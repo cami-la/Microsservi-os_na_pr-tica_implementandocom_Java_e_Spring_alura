346094d65b30b2b413055eaaae0d586a14afe8c4
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2023-07-17 19:43:41 -0300

    Model
    Signed-off-by: Camila Cavalcante <camiladsantoscavalcante@gmail.com>

--- src/main/java/br/com/alurafood/payments/model/Payment.java
+++ src/main/java/br/com/alurafood/payments/model/Payment.java
@@ -6,14 +6,12 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 
 @AllArgsConstructor
+@Builder
 @Entity
 @Getter
 @NoArgsConstructor
