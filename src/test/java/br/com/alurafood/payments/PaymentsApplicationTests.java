7343e2bb0f7d242686aa803a7ecfcc06392ce5b9
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2023-07-17 17:10:15 -0300

    Projeto inicial do curso
    Signed-off-by: Camila Cavalcante <camiladsantoscavalcante@gmail.com>

---
+++ src/test/java/br/com/alurafood/payments/PaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alurafood.payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
