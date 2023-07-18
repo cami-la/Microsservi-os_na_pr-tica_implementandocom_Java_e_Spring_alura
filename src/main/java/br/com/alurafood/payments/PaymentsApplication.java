4f91ce0f14aa64bcde0e006e7d27acb81e8c1352
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2023-07-17 21:09:16 -0300

    Configurando o MS de payment
    Signed-off-by: Camila Cavalcante <camiladsantoscavalcante@gmail.com>

--- src/main/java/br/com/alurafood/payments/PaymentsApplication.java
+++ src/main/java/br/com/alurafood/payments/PaymentsApplication.java
@@ -3,6 +3,7 @@ package br.com.alurafood.payments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class PaymentsApplication {
 
