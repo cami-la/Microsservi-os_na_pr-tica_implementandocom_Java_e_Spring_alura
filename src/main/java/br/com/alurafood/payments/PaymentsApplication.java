95428445e9dfebc7112a33688de5978087826b75
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2023-07-23 22:13:57 -0300

    Circuit breaker e fallback
    Signed-off-by: Camila Cavalcante <camiladsantoscavalcante@gmail.com>

--- src/main/java/br/com/alurafood/payments/PaymentsApplication.java
+++ src/main/java/br/com/alurafood/payments/PaymentsApplication.java
@@ -2,8 +2,9 @@ package br.com.alurafood.payments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-
+@EnableFeignClients
 @SpringBootApplication
 public class PaymentsApplication {
 
--- src/main/java/br/com/alurafood/payments/client/OrderClient.java
+++ src/main/java/br/com/alurafood/payments/client/OrderClient.java
@@ -1,2 +1,14 @@
-package br.com.alurafood.payments.client;public interface OrderClient {
+package br.com.alurafood.payments.client;
+
+import jakarta.validation.constraints.NotNull;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@FeignClient(name = "order-ms")
+public interface OrderClient {
+  @RequestMapping(method = RequestMethod.PUT, value = "/orders/{id}/paid")
+  ResponseEntity<Void> confirmOrderPayment(@PathVariable @NotNull Long id);
 }
